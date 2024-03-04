ad94d82f7c3f7e299517657f107e77d5f86e708c
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2024-03-04 17:40:33 +0530

    Update main.dart

--- dairyconnect/lib/main.dart
+++ dairyconnect/lib/main.dart
@@ -39,13 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
  
     return Scaffold(
       appBar: AppBar(
-       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
         title: Text(widget.title),
       ),
-      body: Center(
-       
+      body: Center
         child: Column(
          
           mainAxisAlignment: MainAxisAlignment.center,
